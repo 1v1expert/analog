c02d85aeb6f9c2990f25bc7420e7ab7a4527c23d
1v1expert <1v1expert@gmail.com>
2019-04-17 17:03:49 +0300

    changed structure view

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .forms import *
 from .models import Product, AttributeValue
 
+
+def render_search(request, queryset):
+	return render(request, 'admin/catalog/search.html', queryset)
 # Create your views here.
+
 
 def search_view(request):
 	form = SearchForm()
@@ -12,7 +16,19 @@ def search_view(request):
 		form = SearchForm(request.POST)
 		if form.is_valid():
 			try:
-				product = Product.objects.get(article=form.cleaned_data['article'], manufacturer=form.cleaned_data['manufacturer_from'])
+				try:
+					product = Product.objects.get(article=form.cleaned_data['article'],
+					                              manufacturer=form.cleaned_data['manufacturer_from'])
+				except Product.DoesNotExist:
+					return render(request, 'admin/catalog/search.html', {'Error': {'val': True,
+					                                                               'msg': 'Не найден продукт с артикулом {}'.format(
+						                                                               form.cleaned_data['article'])}})
+				except Product.MultipleObjectsReturned:
+					return render(request, 'admin/catalog/search.html', {'Error': {'val': True,
+					                                                               'msg': 'Найдено более одного продукта с артикулом {}'
+					                                                               'и производителем {}'.format(
+						                                                        form.cleaned_data['article'],
+						                                                        form.cleaned_data['manufacturer_from'])}})
 				products_values = AttributeValue.objects.filter(product=product)
 				find_products = Product.objects.filter(manufacturer=form.cleaned_data['manufacturer_to'], category=product.category)
 				for attr in products_values:
@@ -28,17 +44,18 @@ def search_view(request):
 					error = {'val': True, 'msg': 'По заданным параметрам не найдено продуктов'}
 				else:
 					error = {'val': True, 'msg': 'По заданным параметрам найдено более одного продукта'}
-				return render(request, 'admin/catalog/search.html', {'Results': find_products,
+				return render(request, 'admin/catalog/search.html', {'Results': find_products, 'Product': product,
 				                                                     'Error': error})
 				# print(products_values, find_product)
 				#print(product.category)
 				#print(product.category.attributes.filter(type='hrd'))
 				#find_product = Product.objects.filter(manufacturer=form.cleaned_data['manufacturer_to'])
 				#print(find_product)
-			except:
-				return render(request, 'admin/catalog/search.html', {'Error': {'val': True, 'msg': 'Произошла ошибка при поиске артикула {}'.format(form.cleaned_data['article'])}})
-				
-			
+			except Exception as e:
+				return render(request, 'admin/catalog/search.html', {'Error': {'val': True,
+				                                                               'msg': 'Произошла непредвиденная ошибка '
+				                                                                      'при поиске артикула {}, >> {}'.format(
+					                                                               form.cleaned_data['article'], e)}})
 			#print(form.cleaned_data)
 		return render(request, 'admin/catalog/search.html', {'Fake': 'Fake'})
 
