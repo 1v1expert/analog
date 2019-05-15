98432e22b685b7e913482d609eeed18a2f6ede3a
1v1expert <1v1expert@gmail.com>
2019-05-16 01:15:30 +0300

    finish render page check_in and login

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from app.views import login_view
+from app.views import login_view, check_in_view
 app_name = 'app'
 urlpatterns = [
-	url(r'^login/$', login_view, name='login')
+	url(r'^login/$', login_view, name='login'),
+	url(r'^check_in/$', check_in_view, name='check_in')
 ]
--- app/views.py
+++ app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def login_view(request):
 	return render(request, 'login.html', {})
+
+
+def check_in_view(request):
+	return render(request, 'check_in.html', {})
