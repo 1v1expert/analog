edec541c0e4a48cb083e847469d61603a53d1fb0
1v1expert <1v1expert@gmail.com>
2019-09-10 03:44:46 +0300

    fix

--- app_2/views.py
+++ app_2/views.py
@@ -8,6 +8,8 @@ def redirect_to_old_template(request):
 
 def view_main_page(request):
 	form = SearchForm()
+	from django.conf import settings
+	print(settings.TEMPLATES)
 	return render(request, 'main.html', {'form': form})
 
 # https://ianlunn.github.io/Hover/
