fdf9e8cf537abc4231e4db977235ce64fafa0817
1v1expert <1v1expert@gmail.com>
2019-12-11 23:48:01 +0300

    remove getex import

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres import fields as pgfields
 from django.utils import timezone
-from django.utils.translation import gettext, gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 import uuid
 
