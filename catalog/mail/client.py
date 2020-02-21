912fe25ba662f77ddc4a1831e13c4349faac9ff7
Vladislav <1v1expert@gmail.com>
2020-02-21 14:34:03 +0300

    upd

--- catalog/mail/client.py
+++ catalog/mail/client.py
@@ -28,14 +28,41 @@ class MailGun(object):
                                    }
                              )
     
-    def send_html_invite(self, to=('info@analogpro.ru', ), html=''):
+    def send_html_invite(self, to=('info@analogpro.ru', ), html='', bcc=("", )):
         html = open(os.path.join(self.PROJECT_ROOT, 'templates/index.html'))
+        images = os.path.join(settings.BASE_DIR, 'app/static/email_images')
         return requests.post(self._url,
                              auth=self._auth,
-                             data={"from": "Vladislav Sazonov <sazonov@analogpro.ru>",
+                             files=[("inline",
+                                     ("ezgif_com-gif-maker.png",
+                                      open(os.path.join(images, "ezgif_com-gif-maker.png"), 'rb').read())),
+                                    ("inline",
+                                     ("search.png",
+                                      open(os.path.join(images, "search.png"), 'rb').read())),
+                                    ("inline",
+                                     ("time.png",
+                                      open(os.path.join(images, "time.png"), 'rb').read())),
+                                    ("inline",
+                                     ("business.png",
+                                      open(os.path.join(images, "business.png"), 'rb').read())),
+                                    ],
+                             data={"from": "АналогПро <sazonov@analogpro.ru>",
                                    "to": to,
-                                   'subject': 'Подбор кабельных лотков - АналогPRO',
-                                   'html': html.read()
+                                   "bcc": bcc,
+                                   'subject': 'Перевод кабельных лотков - БЫСТРО и БЕСПЛАТНО!',
+                                   'html': html.read(),
+                                   'text': '*Инновационный сервис подбора кабельных лотков*\n'
+                                           '*Умный подбор аналогов*\n'
+                                           'Переводите кабельные лотки с одного производителя на другого на сайте '
+                                           'analogpro.ru <http://analogpro.ru>\n'
+                                           '*Быстро и бесплатно*\n'
+                                           'Не тратьте кучу времени и денегна «перевод» спецификаций!'
+                                           'Это можно сделать быстро на analogpro.ru! <http://analogpro.ru>\n'
+                                           '*Для производителей и дистрибьюторов*\n'
+                                           'Вы производитель или дистрибьютор?\n'
+                                           'Разместите виджет analogpro.ru <http://analogpro.ru> на своем сайте БЕСПЛАТНО и '
+                                           'увеличивайте продажи!',
+                                   "o:tracking": True,
                                    }
                              )
 # https://email.uplers.com/blog/step-step-guide-create-html-email/
