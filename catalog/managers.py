acdae0bf6f8a19d06a629b1a2235cca230aaa936
Vladislav <1v1expert@gmail.com>
2020-03-27 05:09:19 +0300

    override queryset delete methode

--- catalog/managers.py
+++ catalog/managers.py
@@ -1,11 +1,20 @@
 from django.db import models
 
 
+class CoreQuerySet(models.query.QuerySet):
+
+    def delete(self):
+        self.update(deleted=True)
+
+    
 class CoreModelManager(models.Manager):
     # TODO make this compatible with related fields.
     # get_queryset returns all instances even that are not related
     # to base model.
 
+    def get_query_set(self):
+        return CoreQuerySet(self.model, using=self._db)
+    
     def get_queryset(self, show_hidden=False):
         qs = super().get_queryset()
         if not show_hidden:
