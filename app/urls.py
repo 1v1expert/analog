f8d529e0a92bfa9aef3b5527faca2e9ea3d63529
Vladislav <1v1expert@gmail.com>
2020-04-12 05:29:34 +0300

    added lang url

--- app/urls.py
+++ app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^old/search_from_file/$', search_from_file_view, name='search_from_file'),
     url(r'^email_confirmation/(?P<verification_code>[0-9\w-]+)-(?P<user_id>[0-9]+)/$',
         landing_confirm_mail_page, name='email_confirmation'),
+    url(r'(?P<lang>[a-z]{2})', landing_page_view, name='landing_home'),
     url(r'^$', landing_page_view, name='landing_home')
 ]
